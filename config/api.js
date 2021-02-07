4a70ec55e9be2ad8eab069555b926d56fafcf93f
Visa Pollari <visa.pollari@gmail.com>
2021-02-08 01:17:17 +0200

    Filter out timestamps

--- config/api.js
+++ config/api.js
@@ -1,6 +1,12 @@
 module.exports = ({ env }) => ({
     responses: {
-        privateAttributes: ["_v", "created_at", "published_at", "updated_at"],
+        privateAttributes: [
+            "_v",
+            "id",
+            "created_at",
+            "published_at",
+            "updated_at",
+        ],
     },
     rest: {
         defaultLimit: 100,
