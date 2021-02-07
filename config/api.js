f47eef1196056b42a50819e8851348cc846c6453
Visa Pollari <visa.pollari@gmail.com>
2021-02-08 00:33:10 +0200

    Add basic Toimari Api

--- config/api.js
+++ config/api.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
     responses: {
-      privateAttributes: ['_v', 'created_at', 'published_at', 'updated_at'],
+        privateAttributes: ["_v", "created_at", "published_at", "updated_at"],
     },
     rest: {
-      defaultLimit: 100,
-      maxLimit: 250,
+        defaultLimit: 100,
+        maxLimit: 250,
     },
 });
