08a6a263d2824b6794b10491c37318a8a31a8a31
Visa Pollari <visa.pollari@gmail.com>
2022-05-22 19:10:06 +0300

    eslint

--- extensions/users-permissions/config/jwt.js
+++ extensions/users-permissions/config/jwt.js
@@ -1,3 +1,3 @@
 module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'c652b793-a07d-4b0c-a701-bc3580c10d08'
-};
+  jwtSecret: process.env.JWT_SECRET || 'c652b793-a07d-4b0c-a701-bc3580c10d08',
+}
--- tests/helpers/strapi.js
+++ tests/helpers/strapi.js
@@ -1,19 +1,19 @@
-const Strapi = require('strapi');
-const http = require('http');
+const Strapi = require('strapi')
+const http = require('http')
 
-let instance;
+let instance
 
 async function setupStrapi() {
   if (!instance) {
     /** the following code in copied from `./node_modules/strapi/lib/Strapi.js` */
-    await Strapi().load();
-    instance = strapi; // strapi is global now
+    await Strapi().load()
+    instance = strapi // strapi is global now
     await instance.app
       .use(instance.router.routes()) // populate KOA routes
-      .use(instance.router.allowedMethods()); // populate KOA methods
+      .use(instance.router.allowedMethods()) // populate KOA methods
 
-    instance.server = http.createServer(instance.app.callback());
+    instance.server = http.createServer(instance.app.callback())
   }
-  return instance;
+  return instance
 }
-module.exports = { setupStrapi };
+module.exports = { setupStrapi }
