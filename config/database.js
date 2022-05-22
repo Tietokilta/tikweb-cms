d48e4567c8ab8fe2d73cf25298d2ea93125a5421
Visa Pollari <visa.pollari@gmail.com>
2022-05-22 20:56:10 +0300

    migrrate to strapi4

--- config/database.js
+++ config/database.js
@@ -1,18 +1,15 @@
+const path = require('path')
+
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: env('DATABASE'),
-        host: env('DATABASE_HOST'),
-        port: env.int('DATABASE_PORT'),
-        database: env('DATABASE_NAME'),
-        username: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
-        ssl: env.bool('DATABASE_SSL'),
-      },
-      options: {},
+  connection: {
+    client: 'sqlite',
+    connection: {
+      filename: path.join(
+        __dirname,
+        '..',
+        env('DATABASE_FILENAME', '.tmp/data.db')
+      ),
     },
+    useNullAsDefault: true,
   },
 })
