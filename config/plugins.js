f41678754c98a5d3165e100afa796da35eca6391
Leevi Heikkilä <leevi.heikkila11@gmail.com>
2022-05-29 17:32:21 +0300

    Media upload to Azure Storage

---
+++ config/plugins.js
@@ -0,0 +1,22 @@
+module.exports = ({ env }) => {
+  // If 'upload' is undefined strapi will use local file system for
+  // file uploads. This is the preferred option in development mode
+  // since the database in docker containing upload URIs doesn't sync with Azure,
+  // and so if the database is reset, it will not show images that are already
+  // previously uploaded to Azure.
+  if (env('NODE_ENV') === 'development') return { upload: undefined }
+
+  // TODO: Add the option 'cdnBaseURL' when the CDN is configured to get the images from the CDN instead of storage account URL.
+  return {
+    upload: {
+      provider: 'azure-storage',
+      providerOptions: {
+        account: env('UPLOADS_ACCOUNT_NAME'),
+        accountKey: env('UPLOADS_ACCOUNT_KEY'),
+        containerName: env('UPLOADS_CONTAINER_NAME'),
+        defaultPath: 'strapi',
+        maxConcurrent: 10,
+      },
+    },
+  }
+}
